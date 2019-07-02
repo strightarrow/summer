0a31d8d13ff2acb3c126820c411b96cb0ba9030e
strightarrow <51964914+strightarrow@users.noreply.github.com>
2019-07-02 14:03:04 -0700

    Add files via upload

---
+++ C++/forloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i,j;
+  for (;;){
+    cout<<i<<" "<<j<<" ";
+    if (i % 5 == o) cout<<endl;
+    i = i + 1;
+    j++;
+    if (j > 1000000) break;
+  }
+  return 0;
+}
---
+++ C++/loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int i;
+  for (i = 0; i <256 ; (i++){
+    cout<<i<<" "<<char(i)<<endl;
+    }
+  return 0
+}
